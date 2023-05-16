4a858ca6825acc2852a460cfe3de7edbb64bdaea
PoQo <95665302+zxvm5962@users.noreply.github.com>
2023-05-16 09:36:21 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define max(a, b) a > b ? a : b
+int d[16], v[16],dp[17];
+
+int main() {
+  int n, day, max = 0;
+  scanf("%d", &n);
+  for (int i = 1; i <= n; i++) {
+    scanf("%d %d", &d[i], &v[i]);
+  }
+  for(int i=n;i>0;i--){
+    day = i + d[i];
+    if(day <= n+1 ){
+      dp[i] = max(dp[day] + v[i], dp[i+1]);
+    }
+    else
+      dp[i] = dp[i+1];
+  }
+
+  printf("%d", dp[1]);
+
+  return 0;
+}
+
+
