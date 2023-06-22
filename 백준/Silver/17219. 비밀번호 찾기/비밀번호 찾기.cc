da8031f118fc088c38aa2589f6554c0bc91fda72
PoQo <95665302+zxvm5962@users.noreply.github.com>
2023-06-22 15:04:30 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 4568 ms, Memory: 16548 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -1,42 +1,18 @@
-#include <algorithm>
-#include <cstring>
 #include <iostream>
-#include <vector>
-
+#include <map>
 using namespace std;
-
-vector<pair<string, string>> v;
-
-int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
-  int n, m;
-  string str1, str2;
-  cin >> n >> m;
-  for (int i = 0; i < n; i++) {
-    cin >> str1 >> str2;
-    v.push_back({str1, str2});
-  }
-  sort(v.begin(), v.end());
-  string key;
-  for (int i = 0; i < m; i++) {
-    cin >> key;
-    int start = 0, end = n - 1;
-    int result;
-    while (start <= end) {
-      int mid = (start + end) / 2;
-      if (key < v[mid].first)
-        end = mid - 1;
-      else if (key > v[mid].first)
-        start = mid + 1;
-      else {
-        result = mid;
-        break;
-      }
+int main(void){
+    int n, m;
+    cin >> n >> m;
+    string address, password;
+    map <string, string> site;
+    string temp;
+    for(int i = 0; i < n; i++){
+        cin >> address >> password;
+        site[address] = password;
     }
-    cout << v[result].second << "\n";
-  }
-
-  return 0;
+    for(int i = 0; i < m; i++){
+        cin >> temp;
+        cout << site[temp] << '\n';
+    }
 }
