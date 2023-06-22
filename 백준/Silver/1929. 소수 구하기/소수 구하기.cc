56fd48a4b15ade28d1e47668b3d7deab91e7da12
PoQo <95665302+zxvm5962@users.noreply.github.com>
2023-06-22 10:07:43 +0900

    [Silver III] Title: 소수 구하기, Time: 40 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -1,19 +1,25 @@
-#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+int prime[1000001];
 int main() {
-  int m,n;
-  int arr[1000001] = {0};
-  scanf("%d %d",&m,&n);
-  arr[1] = 1;
-  for(int i=2;i<=n; i++){
-    for(int j=2; i * j <= n;j++){
-      arr[i*j] =1;
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  int n, m;
+
+  cin >> n >> m;
+
+  prime[1] = 1;
+  for (int i = 2; i < 1000000; i++) {
+    for (int j = 2; i * j < 1000000; j++) {
+      prime[i * j] = 1;
     }
   }
-
-  for (int i=2; i<=n; i++){
-    if(arr[i] == 0 && m<=i){
+  for(int i= n ;i<=m;i++){
+    if(prime[i] == 0)
       printf("%d\n",i);
-    }
   }
   return 0;
 }
