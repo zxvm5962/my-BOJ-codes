491a65b268996f620a9731bf6d0b690ba6a4e59e
PoQo <95665302+zxvm5962@users.noreply.github.com>
2023-06-22 15:21:59 +0900

    [Gold I] Title: 기계오리 연구, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/28082. 기계오리 연구/기계오리 연구.cc
+++ 백준/Gold/28082. 기계오리 연구/기계오리 연구.cc
@@ -6,6 +6,7 @@ using namespace std;
 
 int I[50002];
 int arr[50002];
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(NULL);
@@ -17,22 +18,29 @@ int main() {
   for (int i = 0; i < n; i++) {
     cin >> arr[i];
   }
+
   sort(arr, arr + n);
+
   for (int i = 0; i < n; i++) {
     for (int j = 50001; j > 0; j--) {
-      if (I[j] > 0 && I[j] < k){
-          if(I[j+arr[i]]>0)
-        I[j + arr[i]] = min(I[j] + 1,I[j+arr[i]]);
-          else
-              I[j+arr[i]]=I[j]+1;
-    }}
+      if (I[j] > 0 && I[j] < k) {
+        if (I[j + arr[i]] == 0) {
+          I[j + arr[i]] = I[j] + 1;
+        } 
+        else
+          I[j+ arr[i]] = min (I[j + arr[i]], I[j] +1);
+      }
+    }
     I[arr[i]] = 1;
   }
+
   for (int j = 1; j < 50002; j++) {
     if (I[j] != 0)
       cnt++;
   }
+
   cout << cnt << "\n";
+
   for (int j = 1; j < 50002; j++) {
     if (I[j] != 0)
       cout << j << ' ';
