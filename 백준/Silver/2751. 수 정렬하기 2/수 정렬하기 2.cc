83a3d434d45ed2d8a628cd8d20d60190159d78c3
PoQo <95665302+zxvm5962@users.noreply.github.com>
2023-06-22 10:12:25 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 276 ms, Memory: 8292 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -1,24 +1,26 @@
-#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+vector<int> v;
 int main() {
-  int n,num, arr[1000001] = {0},arr2[1000001] = {0};
-  scanf("%d", &n);
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  int n, num;
+
+  cin >> n;
   for (int i = 0; i < n; i++) {
-    scanf("%d", &num);
-    if(num<0){
-      arr2[~num+1]++;
-    }
-    else arr[num]++;
+    cin >> num;
+    v.push_back(num);
   }
-  for (int i=1000000; i>=0;i--){
-    if (arr2[i]!=0){
-      printf("%d\n",~i+1);
-    }
+  sort(v.begin(), v.end());
+
+  for (int i = 0; i < n; i++) {
+    cout << v[i] << "\n";
   }
-  for (int i=0; i<=1000000;i++){
-    if (arr[i]!=0){
-      printf("%d\n",i);
-    }
-  }
+
   return 0;
 }
