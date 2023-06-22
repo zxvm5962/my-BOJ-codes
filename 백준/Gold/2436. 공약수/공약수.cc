33f7d37f23bb0fa8f2ed0f51bccda1c400009a1e
PoQo <95665302+zxvm5962@users.noreply.github.com>
2023-06-22 12:08:34 +0900

    [Gold V] Title: 공약수, Time: 140 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/2436. 공약수/공약수.cc
+++ 백준/Gold/2436. 공약수/공약수.cc
@@ -1,29 +1,42 @@
-#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <math.h>
+using namespace std;
 
-long long gcd(long long a, long long b) {
+int gcd(int a, int b) {
   if (b == 0)
     return a;
   else
     return gcd(b, a % b);
 }
+
 int main() {
-  long long n, m, min = 100000000, result1, result2;
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-  scanf("%lld %lld", &n, &m);
+  int n, m, sum = 0;
 
+  cin >> n >> m;
   m /= n;
-
-  for (int i = 1; i <= 8000; i++) {
+  int i = sqrt(m);
+  int j, min = 2e8;
+  int mini, minj;
+  while (i <= m) {
     if (m % i == 0) {
-      long long j = m / i;
-      if (gcd(i, j) == 1) {
-        if (min > i + j) {
-          min = i + j;
-          result1 = i;
-          result2 = j;
-        }
+      j = m / i;
+      if (min > i + j && gcd(i, j) == 1) {
+        min = i + j;
+        mini = i;
+        minj = j;
       }
-    }
+      i++;
+    } else
+      i++;
   }
-  printf("%lld %lld", result1 * n, result2 * n);
+  if (mini > minj)
+    printf("%d %d", minj * n, mini * n);
+  else
+    printf("%d %d", mini * n, minj * n);
+  return 0;
 }
