e8962308bc2db3e91a32994984292dc0e36efbc7
PoQo <95665302+zxvm5962@users.noreply.github.com>
2023-06-27 09:06:57 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  string str;
+  cin >> str;
+  cout << ":fan::fan::fan:\n:fan::" << str << "::fan:\n:fan::fan::fan:";
+
+  return 0;
+}
