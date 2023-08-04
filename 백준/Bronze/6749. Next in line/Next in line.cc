7bc18437f06a705a23a219a69110f0511ad72372
PoQo <95665302+zxvm5962@users.noreply.github.com>
2023-08-04 10:00:36 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <cstring>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int a,b;
+  cin >> a >> b;
+  cout << 2*b - a;
+
+  return 0;
+}
