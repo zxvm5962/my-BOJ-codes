e52a886a6a172896d072a411cd1f32e454b1814b
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-06-26 16:25:14 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int x;
+    cin >> x;
+    cout << x * (x - 1);
+
+    return 0;
+}
