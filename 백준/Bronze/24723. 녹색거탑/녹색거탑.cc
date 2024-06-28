d56ec2a30b445d0d6559f227b6ccb70c15a59ce5
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-06-28 12:36:24 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, M, K;
+    cin >> N;
+    cout << pow(2, N);
+}
