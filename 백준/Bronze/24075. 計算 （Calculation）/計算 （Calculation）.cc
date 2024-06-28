abc4f2ed8341662f20aa7b494ed2717f9c0b438e
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-06-28 12:34:03 +0900

    [Bronze IV] Title: 計算 (Calculation), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24075. 計算 （Calculation）/計算 （Calculation）.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, M, K;
+    cin >> N >> M;
+    cout << max(N + M, N - M) << '\n'
+         << min(N + M, N - M);
+}
