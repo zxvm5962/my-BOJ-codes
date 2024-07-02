cae6923cdf3396dc7fce4de811f49a5951fe9c30
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-07-02 16:42:53 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    long long n;
+    cin >> n;
+    long long po;
+    po = pow(n,3);
+    cout << po;
+
+    return 0;
+}
