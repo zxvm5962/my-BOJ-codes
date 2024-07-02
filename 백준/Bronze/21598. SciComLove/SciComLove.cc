eae5d5361c03e17cbe2778854343eaeb254a9a5f
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-07-02 16:59:56 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int T, N, M, K;
+    cin >> T;
+    while(T--){
+        cout << "SciComLove\n";
+    }
+    return 0;
+}
