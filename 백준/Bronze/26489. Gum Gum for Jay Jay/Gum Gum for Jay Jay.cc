f3d63ddfc4e424031398c427d77694b743ebc79b
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-07-03 20:48:52 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long T, N, M, K;
+    string str;
+    int cnt = 0;
+    while(getline(cin, str)){
+        cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
