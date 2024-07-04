db39fa9a9d69b2e81f45a8461f20491f6bd4fcff
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-07-04 17:34:41 +0900

    [Bronze V] Title: Correct, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26307. Correct/Correct.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long T, N, M, K;
+    cin >> N >> M;
+    cout << (N - 9) * 60 + M;
+    return 0;
+}
