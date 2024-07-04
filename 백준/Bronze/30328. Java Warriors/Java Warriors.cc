243fbfa1dbd33770847b90013af9e836b6c192ed
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-07-04 17:44:37 +0900

    [Bronze V] Title: Java Warriors, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+int arr[2];
+
+vector<char> v;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long T, N, M, K;
+    cin >> T;
+    cout << T * 4000;
+}
