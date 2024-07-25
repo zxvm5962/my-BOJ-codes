890c45f3ee8963521acd99eedb8c0bc95351c286
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-07-25 14:38:18 +0900

    [Bronze IV] Title: A Simple Problem., Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15372. A Simple Problem．/A Simple Problem．.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long t, n, m, k, l;
+    cin >> t;
+    while (t--)
+    {
+        int sum;
+        cin >> n;
+        long long a = pow(n,2);
+        cout << a << '\n';
+    }
+    return 0;
+}
