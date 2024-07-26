056736e71af0f5e74daadf13d580cc7c4c20d7da
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-07-26 13:55:22 +0900

    [Unrated] Title: 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/14918. 더하기/더하기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b;
+    cin >> a>> b;
+    cout << a+b;
+    return 0;
+}
