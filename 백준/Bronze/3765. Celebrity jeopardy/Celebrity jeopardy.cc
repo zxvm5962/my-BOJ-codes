c4752351d8210362d5149a17d8193269d155796d
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-07-30 13:57:15 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+
+vector<int> v;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    string str;
+    while (getline(cin, str))
+    {
+        cout << str << '\n';
+    }
+    
+    return 0;
+}
