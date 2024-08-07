07d08e3f74209f2b3aef2f218116b51cd5453ac7
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-08-07 11:12:05 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <map>
+
+using namespace std;
+
+vector<string> v;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, m;
+    string str;
+    cin >> n;
+    cout << n << '\n'
+         << 1;
+    return 0;
+}
