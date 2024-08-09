60ac0a98d96b1838f9f7e74c2071a21482209f17
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-08-09 15:16:22 +0900

    [Bronze III] Title: Hot Dogs, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5666. Hot Dogs/Hot Dogs.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <map>
+
+using namespace std;
+
+vector<pair<int, int>> v;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    long long n, m, k, sum = 0;
+    double a, b, c, d, e, f = 0;
+    while (cin >> a >> b)
+    {
+        cout << fixed;
+        cout.precision(2);
+        cout << a / b << '\n';
+    }
+
+    return 0;
+}
