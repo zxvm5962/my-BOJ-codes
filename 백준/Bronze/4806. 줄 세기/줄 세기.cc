7ce444d96a9edd4887344d162c615adcce141d61
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-08-14 10:59:03 +0900

    [Bronze III] Title: 줄 세기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4806. 줄 세기/줄 세기.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <deque>
+
+using namespace std;
+
+vector<string> v(52);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    long long n, m, k = 0, sum = 0, cnt =0 ;
+    double a, b, c, d, e, f = 0;
+    string str;
+    while(getline(cin, str))
+    {
+        cnt++;
+    }
+    cout << cnt;
+
+    return 0;
+}
