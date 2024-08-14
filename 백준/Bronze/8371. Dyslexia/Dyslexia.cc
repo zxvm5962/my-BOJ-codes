fc8c1fe1d21defb85c003ed47ff83d2b14d9d31c
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-08-14 11:40:26 +0900

    [Bronze III] Title: Dyslexia, Time: 0 ms, Memory: 2528 KB -BaekjoonHub

---
+++ 백준/Bronze/8371. Dyslexia/Dyslexia.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <deque>
+
+using namespace std;
+
+vector<string> v(52);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    long long n, m, k = 0, l, sum = 0, cnt = 0;
+    double a, b, c, d, e, f = 0;
+    string str, s;
+    cin >> t >> str >> s;
+    for (int i = 0; i < t; i++)
+    {
+        if (str[i] != s[i])
+            cnt++;
+    }
+    cout << cnt;
+
+    return 0;
+}
