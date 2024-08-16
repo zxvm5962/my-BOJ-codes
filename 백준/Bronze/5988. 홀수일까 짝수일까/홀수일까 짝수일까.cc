5ec252dddad080e73ea3a4f23e7b0d0a97434dd7
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-08-16 11:13:50 +0900

    [Bronze III] Title: 홀수일까 짝수일까, Time: 0 ms, Memory: 2184 KB -BaekjoonHub

---
+++ 백준/Bronze/5988. 홀수일까 짝수일까/홀수일까 짝수일까.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <queue>
+
+using namespace std;
+
+int arr[201][201];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    long long n, m, k = 0, sum = 0, cnt = 0, oSum = 0;
+    string str, s;
+    cin >> t;
+    while (t--)
+    {
+        cin >> str;
+        if ((str[str.length() - 1] - '0') % 2 == 0)
+            cout << "even\n";
+        else
+            cout << "odd\n";
+    }
+
+    return 0;
+}
