a4c7f5a0032b4b0ed3b99a23a282d0753bafb569
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-08-18 02:06:44 +0900

    [Bronze III] Title: The King, Time: 0 ms, Memory: 2368 KB -BaekjoonHub

---
+++ 백준/Bronze/9806. The King/The King.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <queue>
+
+using namespace std;
+
+int arr[201][201];
+
+vector<long long> v;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    long long n, m, k = 0, sum = 0, cnt = 0, max = 0;
+    string str, s;
+    int min = 1e6;
+    cin >> t;
+    cin >> n;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> m;
+        if (pow(m, n) > 0)
+            sum += pow(m, n);
+    }
+    cout << sum;
+
+    return 0;
+}
