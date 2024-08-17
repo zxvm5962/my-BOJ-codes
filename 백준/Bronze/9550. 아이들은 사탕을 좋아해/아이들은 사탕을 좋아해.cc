ca56f2adeedb19ba2fc2c48208515b0840b1cc28
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-08-18 01:16:25 +0900

    [Bronze III] Title: 아이들은 사탕을 좋아해, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Bronze/9550. 아이들은 사탕을 좋아해/아이들은 사탕을 좋아해.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <queue>
+
+using namespace std;
+
+int arr[201][201];
+
+vector<long long> v;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    long long n, m, k = 0, sum = 0, cnt = 0, max = 0;
+    string str, s;
+
+    cin >> t;
+    while (t--)
+    {
+        cin >> n >> k;
+        sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> m;
+            sum += m / k;
+        }
+
+        cout << sum << '\n';
+    }
+    return 0;
+}
