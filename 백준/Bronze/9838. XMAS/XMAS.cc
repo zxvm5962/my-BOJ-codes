f6e574cf65d178650aafbee5485949a0a08c9127
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-08-18 02:10:46 +0900

    [Bronze III] Title: XMAS, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Bronze/9838. XMAS/XMAS.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <queue>
+
+using namespace std;
+
+vector<long long> v(20001);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    long long n, m, k = 0, sum = 0, cnt = 0, max = 0;
+    string str, s;
+    int min = 1e6;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        cin >> n;
+        v[n] = i;
+    }
+    for (int i = 1; i <= t; i++)
+    {
+        cout << v[i] << '\n';
+    }
+    
+
+    return 0;
+}
