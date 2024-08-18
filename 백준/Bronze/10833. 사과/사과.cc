35f1718dbd210ec6c30fc89955bba1dbc0e3bf49
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-08-18 16:13:33 +0900

    [Bronze III] Title: 사과, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10833. 사과/사과.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <queue>
+
+using namespace std;
+
+vector<long long> v;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    long long n, m, k = 0, sum = 0, cnt = 0, max = 0;
+    string str, s;
+    int min = 1e6;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n >> m;
+        sum += m % n;
+    }
+    cout << sum;
+    return 0;
+}
