4fc6553d3d69ae0e401774e4ad810db247983c6c
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-08-18 12:25:49 +0900

    [Bronze III] Title: The Euclidean Algorithm, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Bronze/9924. The Euclidean Algorithm/The Euclidean Algorithm.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <queue>
+
+using namespace std;
+
+vector<long long> v(20001);
+
+int gcd(int n, int m, int cnt)
+{
+    int r, temp;
+
+    while (n != m)
+    {
+        temp = max(n, m);
+        m = min(n, m);
+        n = temp;
+
+        n = n - m;
+        cnt++;
+    }
+    return cnt;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    long long n, m, k = 0, sum = 0, cnt = 0, max = 0;
+    string str, s;
+    int min = 1e6;
+    cin >> n >> m;
+
+    cout << gcd(n, m, cnt);
+    return 0;
+}
