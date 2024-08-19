10f8ad75387b5f416ff34251cbcbb30ebf85a44d
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-08-19 13:51:45 +0900

    [Bronze III] Title: Chanukah Challenge, Time: 4 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Bronze/15295. Chanukah Challenge/Chanukah Challenge.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <queue>
+
+using namespace std;
+
+vector<long long> v(100001);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    long long n, m, k = 0, sum = 0, cnt = 0, max = 0;
+    string str, s;
+    int min = 1e6;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n >> m;
+        cout << n << ' ' << (m + 1) * (m + 2) / 2 - 1 << '\n';
+    }
+    return 0;
+}
