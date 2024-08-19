650537d5dd54472a0378e694fec85db82e8792cc
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-08-19 14:20:26 +0900

    [Bronze III] Title: 전체 계산 횟수, Time: 0 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Bronze/17174. 전체 계산 횟수/전체 계산 횟수.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <queue>
+
+using namespace std;
+
+vector<long long> v(100001);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    long long n, m, k = 0, sum = 0, cnt = 0, max = 0;
+    string str, s;
+    int min = 1e6;
+    cin >> n >> m;
+    sum += n;
+    while (n / m > 0)
+    {
+        sum += n / m;
+        n /= m;
+    }
+    cout << sum;
+
+    return 0;
+}
