ae37d7c5596f8c94aa9714e94fd0307583b84e4b
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-08-19 14:08:42 +0900

    [Bronze III] Title: Nadan, Time: 0 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Bronze/16239. Nadan/Nadan.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <queue>
+
+using namespace std;
+
+vector<long long> v(100001);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    long long n, m, k = 0, sum = 0, cnt = 0, max = 0;
+    string str, s;
+    int min = 1e6;
+    cin >> n >> t;
+    for (int i = 1; i < t; i++)
+    {
+        n -= i;
+        cout << i << '\n';
+    }
+    cout << n;
+
+    return 0;
+}
