6348f994206d31f158d91ed02871d4a6ebb88518
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-08-20 13:38:13 +0900

    [Bronze III] Title: 서버, Time: 0 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Bronze/10409. 서버/서버.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <queue>
+
+using namespace std;
+
+vector<long long> v(100001);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    long long n, m, k = 0, sum = 0, cnt = 0, max = 0, c, p;
+    string str, s;
+    double a, b;
+    cin >> t >> n;
+    while (t--)
+    {
+        cin >> m;
+        n -= m;
+        if (n < 0)
+            break;
+        cnt++;
+    }
+    cout << cnt;
+}
