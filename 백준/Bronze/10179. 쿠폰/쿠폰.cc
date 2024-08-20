59a1bdad11a73ed623c95cafa906e69b3d2895af
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-08-20 13:27:08 +0900

    [Bronze III] Title: 쿠폰, Time: 0 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Bronze/10179. 쿠폰/쿠폰.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <queue>
+
+using namespace std;
+
+vector<long long> v(100001);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    long long n, m, k = 0, sum = 0, cnt = 0, max = 0, c, p;
+    string str, s;
+    cin >> t;
+    double a;
+    while (t--)
+    {
+        cin >> a;
+        cout << fixed;
+        cout.precision(2);
+        cout << '$' << a * 0.8 << '\n';
+    }
+}
