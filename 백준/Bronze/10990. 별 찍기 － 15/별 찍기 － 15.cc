c91cc1c80ad46a4fe82e3d90512772befccb1305
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-08-20 14:33:08 +0900

    [Bronze III] Title: 별 찍기 - 15, Time: 0 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Bronze/10990. 별 찍기 － 15/별 찍기 － 15.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <map>
+#include <deque>
+#include <queue>
+
+using namespace std;
+
+vector<long long> v(100001);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    long long n, m, k = 0, sum = 0, cnt = 0, max = 0, c, p;
+    string str, s;
+    double a, b = 0;
+
+    cin >> t;
+    if (t == 1)
+    {
+        cout << "*";
+        return 0;
+    }
+    for (int i = 0; i < t; i++)
+    {
+        for (int j = t; j > i + 1; j--)
+        {
+            cout << ' ';
+        }
+        cout << "*";
+        for (int j = 0; j < 2 * i - 1; j++)
+        {
+            cout << ' ';
+        }
+        if (i > 0)
+            cout << "*\n";
+        else
+            cout << '\n';
+    }
+}
