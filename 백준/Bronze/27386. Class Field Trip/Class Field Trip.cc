a67ef18dc11b1c3e301e7f9dfc3a74a54b6effd1
PoQo <95665302+zxvm5962@users.noreply.github.com>
2024-08-30 13:18:45 +0900

    [Bronze II] Title: Class Field Trip, Time: 4 ms, Memory: 9840 KB -BaekjoonHub

---
+++ 백준/Bronze/27386. Class Field Trip/Class Field Trip.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <sstream>
+#include <map>
+#include <deque>
+#include <queue>
+
+using namespace std;
+
+vector<long long> v(1000001);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    long long n, m, k = 0, sum = 0, cnt = 0, maxV = 0, c, p = 0, minV = 1e8;
+    string str, s;
+    double a, b = 0;
+    vector<double> w;
+
+    cin >> str;
+    for (int i = 0; i < str.length(); i++)
+    {
+        v[str[i]]++;
+    }
+
+    cin >> str;
+    for (int i = 0; i < str.length(); i++)
+    {
+        v[str[i]]++;
+    }
+    for (int i = 'a'; i <= 'z'; i++)
+    {
+        while (v[i]--)
+            cout << (char)i;
+    }
+}
